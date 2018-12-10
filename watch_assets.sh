f67c5b412f864d1b3ceb6ac22cfb2edff2bc0620
TaeHwan Eun <theun@postech.ac.kr>
2018-12-10 19:36:38 +0900

    헤더 스타일을 frontend와 맞춤

--- watch_assets.sh
+++ watch_assets.sh
@@ -1,1 +1,1 @@
-paver watch_assets --theme-dirs=/edx/app/edxapp/edx-platform/themes --themes=edux 2>&1 | tee -a watch_assets.log
+paver watch_assets --theme-dirs=/edx/app/edxapp/themes/edx-platform --themes=edux 2>&1 | tee -a watch_assets.log
